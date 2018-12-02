6b3ebbee2bd55ad16d94e6df3a5f234c16350eb6
hanqyu <peakjnut37@gmail.com>
2018-12-03 01:35:28 +0900

    Update deploy.sh

--- .config/deploy.sh
+++ .config/deploy.sh
@@ -4,7 +4,7 @@ sudo cp -f ~/srv/hanqyu-tools/.config/nginx-app.conf /etc/nginx/sites-available/
 sudo ln -sf /etc/nginx/sites-available/nginx-app.conf /etc/nginx/sites-enabled/nginx-app.conf
 sudo cp -f ~/srv/hanqyu-tools/.config/uwsgi.service /etc/systemd/system/uwsgi.service
 
-cd /srv/app
+cd /srv/hanqyu-tools
 /bin/bash -c \
 '/home/ubuntu/.pyenv/versions/3.6.5/envs/ec2-deploy/bin/python \
 ~/srv/hanqyu-tools/manage.py collectstatic --noinput' ubuntu
