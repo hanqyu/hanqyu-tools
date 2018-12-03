5e81aa7333735f4e3e064557380d44ccd63ed3de
hanqyu <peakjnut37@gmail.com>
2018-12-03 22:50:11 +0900

    update models.py

--- tools/models.py
+++ tools/models.py
@@ -46,19 +46,11 @@ class Photo(models.Model):
         # Load the jpg file into a numpy array
         image = face_recognition.load_image_file(image_object)
 
-        # Find all the faces in the image using the default HOG-based model.
-        # This method is fairly accurate, but not as accurate as the CNN model and not GPU accelerated.
-        # See also: find_faces_in_picture_cnn.py
         face_locations = face_recognition.face_locations(image)
 
-        # print("I found {} face(s) in this photograph.".format(len(face_locations)))
-
-        # for face_location in face_locations:
-
+        # for face_location in face_locations: # for multiple faces
         face_location = face_locations[0]
-        # Print the location of each face in this image
         top, right, bottom, left = face_location
-        # print("A face is located at pixel location Top: {}, Left: {}, Bottom: {}, Right: {}".format(top, left, bottom, right))
 
         vertical, horizontal = [axis == min(original_image_size) for axis in original_image_size]
 
